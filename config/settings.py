ec70b3637f82800a4d050127be8ce0ba2a4c7ab9
loliette <yelmaniyel@gmail.com>
2026-02-24 13:08:18 +0000

    responsive en cours

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok-free.app']
 
 # Application definition
 
--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('sales/', include('sales.urls', namespace='sales')),
     path('forms/', include('kibo_forms.urls',namespace='kibo_forms')),
     ##
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('login/', auth_views.LoginView.as_view(), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
